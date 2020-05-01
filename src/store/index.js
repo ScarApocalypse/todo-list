c94a19249976ea50f2d93c92cb8c2a0f111f7728
Scarborough <740984027@qq.com>
2020-05-01 23:10:13 +0800

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,85 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    // 所有的任务列表
+    list: [],
+    // 文本框的内容
+    inputValue: 'aaa',
+    // 下一个id
+    nextId: 5,
+    viewKey: 'all'
+  },
+  mutations: {
+    initList(state, list) {
+      state.list = list
+    },
+    // 为state中的inputvalue赋值
+    setInputValue(state, val) {
+      state.inputValue = val
+    },
+    // 添加列表项
+    addItem(state) {
+      const obj = {
+        id: state.nextId,
+        info: state.inputValue.trim(),
+        done: false
+      }
+      state.list.push(obj)
+      state.nextId++
+      state.inputValue = ''
+      state.viewKey = 'all'
+    },
+    // 删除列表项
+    removeItem(state, id) {
+      const i = state.list.findIndex(x => x.id === id)
+      if (i !== -1) {
+        state.list.splice(i, 1)
+      }
+    },
+    // 修改列表项的选中状态
+    changeStatus(state, param) {
+      const i = state.list.findIndex(x => x.id === param.id)
+      if (i !== -1) {
+        state.list[i].done = param.status
+      }
+      console.log(state.list)
+    },
+    // 清除已完成任务
+    clearDone(state) {
+      state.list = state.list.filter(x => x.done === false)
+    },
+    // 修改视图的关键字
+    changeViewKey(state, key) {
+      state.viewKey = key
+    }
+  },
+  actions: {
+    getList(context) {
+      axios.get('/list.json').then(({ data }) => {
+        console.log(data)
+        context.commit('initList', data)
+      })
+    }
+  },
+  getters: {
+    // 统计未完成的任务条数
+    unDoneLength(state) {
+      return state.list.filter(x => x.done === false).length
+    },
+    infolist(state) {
+      if (state.viewKey === 'all') {
+        return state.list
+      } else if (state.viewKey === 'undone') {
+        return state.list.filter(x => x.done === false)
+      } else {
+        return state.list.filter(x => x.done === true)
+      }
+    }
+  },
+  modules: {}
+})
